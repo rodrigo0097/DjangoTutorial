c15a765d7fe59087550afadc6edc1e6ac8e54cef
rodrigo0097 <30930665+rodrigo0097@users.noreply.github.com>
2021-04-10 00:54:51 -0500

    changes

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question, Choice
 from django.template import loader
 from django.http import Http404
 from django.shortcuts import get_object_or_404
+from django.urls import reverse
 """
 ##
 función curda - sin template
@@ -34,6 +35,7 @@ def index(request):
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
+
 """
 ###
 el método que se útiliza abajo o implementa get_object_or_404, por lo que se 
@@ -60,5 +62,45 @@ def results(request, question_id):
     return HttpResponse(response % question_id)
 
 
+"""
+implementación generica del método vote, se útilizo solamente para entender el http response
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+"""
+
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+    """ As the Python comment above points out, you should always return an HttpResponseRedirect
+     after successfully dealing with POST data. This tip isn’t specific to Django; 
+     it’s good Web development practice in general.
+    """
+
+    """
+    We are using the reverse() function in the HttpResponseRedirect constructor in this example.
+     This function helps avoid having to hardcode a URL in the view function. 
+     It is given the name of the view that we want to pass control to and the variable portion 
+     of the URL pattern that points to that view. In this case, using the URLconf we set up in Tutorial 3,
+      this reverse() call will return a string like '/polls/3/results/'
+    """
+
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
