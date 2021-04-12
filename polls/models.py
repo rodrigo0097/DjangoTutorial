3addb5d28d47e5434b9633f765021c3325f88790
rodrigo0097 <30930665+rodrigo0097@users.noreply.github.com>
2021-04-12 11:00:44 -0500

    the first test case is created, also the was_published_recently method is updated to solve the bug that allowed to publish Questions with future dates

--- polls/models.py
+++ polls/models.py
@@ -11,8 +11,14 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
+    """
+    el método es corregido para que no tenga el bug de que se puedan crear preguntas con fechas superiores a la actual
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    """
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
